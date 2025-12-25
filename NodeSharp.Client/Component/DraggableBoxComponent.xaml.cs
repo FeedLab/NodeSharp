ace13f7a46b66ca01955a2a121ef60d2041e267e
Jonny Nilsson <jompa67@gmail.com>
2025-12-25 09:58:34 +0700

    c

---
+++ NodeSharp.Client/Component/DraggableBoxComponent.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NodeSharp.Client.Component;
+
+public partial class DraggableBoxComponent : ContentView
+{
+    double startX, startY;
+
+    public static readonly BindableProperty BoxColorProperty =
+        BindableProperty.Create(
+            nameof(BoxColor),
+            typeof(Color),
+            typeof(DraggableBoxComponent),
+            Colors.Gray);
+
+    public Color BoxColor
+    {
+        get => (Color)GetValue(BoxColorProperty);
+        set => SetValue(BoxColorProperty, value);
+    }
+
+    public DraggableBoxComponent()
+    {
+        InitializeComponent();
+
+        var panGesture = new PanGestureRecognizer();
+        panGesture.PanUpdated += OnPanUpdated;
+        GestureRecognizers.Add(panGesture);
+    }
+
+    void OnPanUpdated(object sender, PanUpdatedEventArgs e)
+    {
+        if (Parent is not AbsoluteLayout)
+            return;
+
+        switch (e.StatusType)
+        {
+            case GestureStatus.Started:
+                var bounds = AbsoluteLayout.GetLayoutBounds(this);
+                startX = bounds.X;
+                startY = bounds.Y;
+                break;
+
+            case GestureStatus.Running:
+                AbsoluteLayout.SetLayoutBounds(
+                    this,
+                    new Rect(
+                        startX + e.TotalX,
+                        startY + e.TotalY,
+                        Width,
+                        Height));
+                break;
+        }
+    }
+}
--- NodeSharp.Client/MainPage.xaml.cs
+++ NodeSharp.Client/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using NodeSharp.Client.Services;
+﻿using Microsoft.Maui.Layouts;
+using NodeSharp.Client.Component;
+using NodeSharp.Client.Services;
 using NodeSharp.Client.ViewModel;
 
 namespace NodeSharp.Client;
@@ -16,5 +18,24 @@ public partial class MainPage : ContentPage
         this.BindingContext = viewModel;
         
         viewModel.Init();
+        
+        AddBox(50, 50, Colors.Red);
+        AddBox(200, 50, Colors.Green);
+        AddBox(350, 50, Colors.Blue);
+    }
+    
+    void AddBox(double x, double y, Color color)
+    {
+        var box = new DraggableBoxComponent
+        {
+            BoxColor = color,
+            WidthRequest = 100,
+            HeightRequest = 100
+        };
+
+        AbsoluteLayout.SetLayoutBounds(box, new Rect(x, y, 100, 100));
+        AbsoluteLayout.SetLayoutFlags(box, AbsoluteLayoutFlags.None);
+
+        Canvas.Children.Add(box);
     }
 }
