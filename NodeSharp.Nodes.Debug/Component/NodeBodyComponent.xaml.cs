3117d01e1cb42cb1e8a937a1c3231cb6ed1cb2a1
Jonny Nilsson <jompa67@gmail.com>
2026-01-29 09:49:48 +0700

    Switch in Debug node

---
+++ NodeSharp.Nodes.Debug/Component/NodeBodyComponent.xaml.cs
@@ -0,0 +1,72 @@
+﻿using CommunityToolkit.Maui;
+using CommunityToolkit.Mvvm.Messaging;
+using NodeSharp.Nodes.Common;
+using NodeSharp.Nodes.Common.Model;
+using NodeSharp.Nodes.Common.Services;
+using NodeSharp.Nodes.Common.ViewModels;
+using Syncfusion.Maui.Buttons;
+
+namespace NodeSharp.Nodes.Debug.Component;
+
+public partial class NodeBodyComponent : ContentView
+{
+    private readonly IPopupService popupService;
+    private readonly DefaultBoxNodeBodyViewModel viewModel;
+
+    internal SfSwitch ShowOutputMessageSwitch => ShowOutputMessage;
+
+    public NodeBodyComponent(BaseNode node)
+    {
+        viewModel = new DefaultBoxNodeBodyViewModel(node);
+
+        InitializeComponent();
+
+        this.BindingContext = viewModel;
+
+        popupService = AppService.GetRequiredService<IPopupService>();
+
+        SizeChanged += (sender, args) =>
+        {
+            InvalidateMeasure();
+        };
+    }
+
+    private void OnSymbolPointerEntered(object? sender, PointerEventArgs e)
+    {
+        if (viewModel?.Node?.Explanations.Count > 0)
+        {
+            SymbolLabel.BackgroundColor = Colors.LightBlue;
+            SymbolLabel.Opacity = 0.8;
+        }
+    }
+
+    private void OnSymbolPointerExited(object? sender, PointerEventArgs e)
+    {
+        SymbolLabel.BackgroundColor = Colors.Transparent;
+        SymbolLabel.Opacity = 1.0;
+    }
+
+    private async void OnSymbolDoubleTapped(object? sender, TappedEventArgs e)
+    {
+        if (viewModel?.Node?.Explanations.Count > 0)
+        {
+            var baseNode = viewModel.Node;
+            var queryAttributes = new Dictionary<string, object>
+            {
+                [nameof(BaseNode)] = baseNode
+            };
+
+            var popupOptions = new PopupOptions
+            {
+                CanBeDismissedByTappingOutsideOfPopup = false
+            };
+            
+            await popupService.ShowPopupAsync<NodeExplanationsPopupViewModel>(
+                Shell.Current,
+                options: popupOptions,
+                shellParameters: queryAttributes);
+            
+            WeakReferenceMessenger.Default.Send(new ShowExplanationsMessage(viewModel.Node));
+        }
+    }
+}
